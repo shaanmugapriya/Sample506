6fdd3a0114b3bd71d3bceb169bf6fac67b198f62
shaanmugapriya <shaanmugapriya@github.com>
2024-02-27 13:57:34 +0530

    Updated Files

--- WebApplication/obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
+++ WebApplication/obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da19e36a4f55a077c5354621633706b64dd92aff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3350c48641762c5691d754570d3d93b9feb54e84")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
